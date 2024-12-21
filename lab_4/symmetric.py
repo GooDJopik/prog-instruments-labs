86e3149774ee92c5eac64bb9a5661ecc009784b8
Goodjop <sashakinser2005@mail.ru>
2024-12-21 06:57:19 +0400

    completion of logging

--- lab_4/symmetric.py
+++ lab_4/symmetric.py
@@ -2,6 +2,9 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from works_files import read_bytes, write_bytes_text, write_file
+from logger_config import setup_logger
+
+logger = setup_logger(__name__)
 
 
 class Symmetric:
@@ -15,6 +18,7 @@ class Symmetric:
     def __init__(self, key_length: int = 256):
         self.key_length = key_length
         self.key = None
+        logger.info("Symmetric instance created.")
 
     def generate_key(self, size_key: int) -> bytes:
         """
@@ -26,10 +30,12 @@ class Symmetric:
         Returns:
         bytes: The generated key.
         """
+        logger.info(f"Generating symmetric key with size: {size_key} bits.")
         if size_key not in [128, 192, 256]:
+            logger.error(f"Invalid key length: {size_key}. Please choose 128, 192, or 256 bits.")
             raise ValueError("Invalid key length. Please choose 128, 192, or 256 bits.")
         self.key = b'7\xda\\nxvMwR\x0cff\xd8h\x0c76\xa4)\xf1\xed\xac\x04\xae\x81\x1b\xae\x11\x1az\x94\xe5\xe6\xec\xc0='
-
+        logger.info("Symmetric key generated.")
         print(self.key)
         return self.key
 
@@ -40,12 +46,16 @@ class Symmetric:
         Parameters:
             file_name: The path to the file containing the encryption key.
         """
+        logger.info(f"Deserializing symmetric key from: {file_name}")
         try:
             with open(file_name, "rb") as file:
                 self.key = file.read()
+            logger.info(f"Symmetric key successfully deserialized from: {file_name}")
         except FileNotFoundError:
+            logger.error(f"File not found: {file_name}")
             print("The file was not found")
         except Exception as e:
+            logger.error(f"An error occurred while reading the file: {str(e)}")
             print(f"An error occurred while reading the file: {str(e)}")
 
     def serialize_sym_key(self, path: str) -> None:
@@ -55,13 +65,17 @@ class Symmetric:
         Parameters:
             path: The path to the file where the encryption key will be saved.
         """
+        logger.info(f"Serializing symmetric key to: {path}")
         try:
             with open(path, 'wb') as key_file:
                 key_file.write(self.key)
+            logger.info(f"Symmetric key successfully written to file: {path}")
             print(f"The symmetric key has been successfully written to the file '{path}'.")
         except FileNotFoundError:
+            logger.error(f"File not found: {path}")
             print("The file was not found")
         except Exception as e:
+            logger.error(f"An error occurred while writing the file: {str(e)}")
             print(f"An error occurred while writing the file: {str(e)}")
 
     def encrypt(self, path_text: str, encrypted_path_text: str) -> bytes:
@@ -75,6 +89,7 @@ class Symmetric:
         Returns:
             The encrypted data.
         """
+        logger.info(f"Encrypting data from: {path_text} to: {encrypted_path_text}")
         text = read_bytes(path_text)
         iv = os.urandom(16)
         cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv))
@@ -85,6 +100,7 @@ class Symmetric:
 
         cipher_text = iv + encryptor.update(padded_text) + encryptor.finalize()
         write_bytes_text(encrypted_path_text, cipher_text)
+        logger.info(f"Data successfully encrypted and written to: {encrypted_path_text}")
         return cipher_text
 
     def decrypt(self, encrypted_path_text: str, decrypted_path_text: str) -> str:
@@ -98,10 +114,12 @@ class Symmetric:
         Returns:
             The decrypted data as a string.
         """
+        logger.info(f"Decrypting data from: {encrypted_path_text} to: {decrypted_path_text}")
         encrypted_text = read_bytes(encrypted_path_text)
         iv = encrypted_text[:16]
         cipher_text = encrypted_text[16:]
-        cipher = Cipher(algorithms.AES(b'7\xda\\nxvMwR\x0cff\xd8h\x0c76\xa4)\xf1\xed\caf\x06\xae\x81\x1b\xae\x11\x1az\x94\xe5\xe6\xec\xc0='), modes.CBC(iv))
+        cipher = Cipher(algorithms.AES(b'7\xda\\nxvMwR\x0cff\xd8h\x0c76\xa4)\xf1\xed'
+                                       b'\caf\x06\xae\x81\x1b\xae\x11\x1az\x94\xe5\xe6\xec\xc0='), modes.CBC(iv))
         decryptor = cipher.decryptor()
         d_text = decryptor.update(cipher_text) + decryptor.finalize()
 
@@ -110,4 +128,5 @@ class Symmetric:
 
         d_text = unpadded_dc_text.decode('UTF-8')
         write_file(decrypted_path_text, d_text)
+        logger.info(f"Data successfully decrypted and written to: {decrypted_path_text}")
         return d_text
--- lab_4/works_files.py
+++ lab_4/works_files.py
@@ -1,4 +1,7 @@
 import json
+from logger_config import setup_logger
+
+logger = setup_logger(__name__)
 
 
 def read_json(path: str) -> dict:
@@ -10,14 +13,19 @@ def read_json(path: str) -> dict:
     Returns
         Dictionary of data from a JSON file
     """
+    logger.info(f"Reading JSON data from: {path}")
     try:
         with open(path, 'r', encoding='UTF-8') as file:
             data = json.load(file)
+        logger.info(f"JSON data successfully read from: {path}")
         return data
     except FileNotFoundError:
+        logger.error(f"File not found: {path}")
         print("The file was not found")
     except Exception as e:
+        logger.error(f"An error occurred while reading the JSON file: {str(e)}")
         print(f"An error occurred while reading the JSON file: {str(e)}")
+        return None
 
 
 def write_file(path: str, data: str) -> None:
@@ -28,13 +36,17 @@ def write_file(path: str, data: str) -> None:
         path: the path to the file to write
         data: data to write to a file
     """
+    logger.info(f"Writing data to file: {path}")
     try:
         with open(path, "w", encoding='UTF-8') as file:
             file.write(data)
+        logger.info(f"Data successfully written to file: {path}")
         print(f"The data has been successfully written to the file '{path}'.")
     except FileNotFoundError:
+        logger.error(f"File not found: {path}")
         print("The file was not found")
     except Exception as e:
+        logger.error(f"An error occurred while writing the file: {str(e)}")
         print(f"An error occurred while writing the file: {str(e)}")
 
 
@@ -47,14 +59,19 @@ def read_bytes(file_path: str) -> bytes:
     Returns
         The contents of the file in binary format.
     """
+    logger.info(f"Reading binary data from: {file_path}")
     try:
         with open(file_path, "rb") as file:
             data = file.read()
+        logger.info(f"Binary data successfully read from: {file_path}")
         return data
     except FileNotFoundError:
+        logger.error(f"File not found: {file_path}")
         print("The file was not found")
     except Exception as e:
+        logger.error(f"An error occurred while reading the file: {str(e)}")
         print(f"An error occurred while reading the file: {str(e)}")
+        return b''
 
 
 def write_bytes_text(file_path: str, bytes_text: bytes) -> None:
@@ -65,11 +82,15 @@ def write_bytes_text(file_path: str, bytes_text: bytes) -> None:
         file_path: The path to the file where the data will be written.
         bytes_text: The binary data to be written to the file.
     """
+    logger.info(f"Writing binary data to file: {file_path}")
     try:
         with open(file_path, "wb") as file:
             file.write(bytes_text)
+        logger.info(f"Binary data successfully written to file: {file_path}")
         print(f"The data has been successfully written to the file '{file_path}'.")
     except FileNotFoundError:
+        logger.error(f"File not found: {file_path}")
         print("The file was not found")
     except Exception as e:
+        logger.error(f"An error occurred while writing the file: {str(e)}")
         print(f"An error occurred while writing the file: {str(e)}")
